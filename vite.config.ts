100c6cf5455a9e3ff2be7589fada6d5e44f77520
Jim Huang <jim.huang112@gmail.com>
2023-08-28 17:46:15 +0800

    :package: chore: add vite config base url for github-page

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/vite-react-jest/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/vite-react-template/" : "/", // for github pages setting base path
 });
