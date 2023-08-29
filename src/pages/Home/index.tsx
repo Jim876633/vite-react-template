5f895501b4cbdc800ac5f4c56a1a0186a3aa09f6
Jim Huang <jim.huang112@gmail.com>
2023-08-29 17:18:49 +0800

    :package: chore: add vite typescript alias

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import reactLogo from "../../assets/react.svg";
+import reactLogo from "@/assets/react.svg";
 import viteLogo from "/vite.svg";
 import { Link } from "react-router-dom";
 const Home = () => {
