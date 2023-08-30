ad6b29c64afc9d363006dcf8cff2c31ce8204b80
Jim Huang <jim.huang112@gmail.com>
2023-08-30 23:02:46 +0800

    :package: chore: add test(viteset) and ts alias library
    - vitest
    - @testing-library
    - jest-dom
    - react
    - user-event
    - vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -6,4 +7,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   base: process.env.NODE_ENV === "production" ? "/vite-react-template/" : "/", // for github pages setting base path
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
+  },
 });
