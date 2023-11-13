b2d1906b42c6b9992333d6fedb728670b25c15fd
Jim Huang <jim.huang112@gmail.com>
2023-11-13 10:20:03 +0800

    :bug: fix: fix commitlint not match rule

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,6 +2,21 @@ module.exports = {
   extends: ["cz", "gitmoji"],
   rules: {
     "type-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "chore",
+        "docs",
+        "style",
+        "refactor",
+        "test",
+        "revert",
+        "update",
+      ],
+    ],
     "type-min-length": [2, "always", 3],
     "subject-empty": [2, "never"],
   },
